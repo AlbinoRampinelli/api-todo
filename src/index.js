0f97419dbb11817f4833cd60a173a26d396ed66c
Albino <albino@dds.com.br>
2021-08-27 08:47:39 -0300

    tirei a variavel Mongo_uri

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express())
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true,  useUnifiedTopology: true}, (err) => {
+mongoose.connect('mongodb+srv://todo:todo@cluster0.pqfvb.mongodb.net/todoapp?retryWrites=true&w=majority', { useNewUrlParser: true,  useUnifiedTopology: true}, (err) => {
     if(err) return console.log('erro de conexão com o database');
     console.log('Connected to database')
 });
